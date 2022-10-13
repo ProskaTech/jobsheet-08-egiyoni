d3e3b2ed82e6c6d0c267b9342669b96ef998c4ad
Egi Yoni Sandra <112674922+egiyoni@users.noreply.github.com>
2022-10-13 19:54:12 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/tugas/js08/ArraySampel.java
@@ -0,0 +1,15 @@
+package com.mycompany.tugas.js08;
+
+/**
+ *
+ * @author 21343005_EGI YONI SANDRA
+ */
+
+public class ArraySampel {
+    public static void main(String[] args) {
+        int[] ages = new int[100];
+        for (int i = 0; i < 100; i++ ) {
+            System.out.print(ages[i]);
+        }
+    }    
+}
---
+++ src/main/java/com/mycompany/tugas/js08/ArraySample2.java
@@ -0,0 +1,15 @@
+package com.mycompany.tugas.js08;
+
+/**
+ *
+ * @author 21343005_EGI YONI SANDRA
+ */
+
+public class ArraySample2 {
+    public static void main(String[] args) {
+        int[] ages = new int[100]; //ini untuk menampilkan angka 0 sampai seratus buah
+        for (int i = 0; i < ages.length; i++) {
+            System.out.print(ages[i]);
+        }
+    } 
+}
